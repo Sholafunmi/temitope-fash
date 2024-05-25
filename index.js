bbe3120cf99c993ad0b827eba3f8a66f8787c03a
Sholafunmi Adewara <sholafunmiadewara@gmail.com>
2024-05-25 13:03:15 +0100

    date auto update

--- index.js
+++ index.js
@@ -4,38 +4,42 @@ const closeSideBar = document.querySelector(".close-menu-bar");
 const sideMenuItems = document.querySelector(".side-menu");
 let bar = [];
 
+showSidebar.addEventListener("click", function () {
+  sideMenuBar.style.transform = "translate(0%)";
+});
 
-showSidebar.addEventListener("click", function(){
-sideMenuBar.style.transform = "translate(0%)"
-}) 
+// close side bar
+closeSideBar.addEventListener("click", function () {
+  sideMenuBar.style.transform = "translate(100%)";
+});
 
-
-// close side bar 
-closeSideBar.addEventListener("click", function(){
-    sideMenuBar.style.transform = "translate(100%)"
-})
-
-sideMenuItems.addEventListener("click", function(){
-    sideMenuBar.style.transform = "translate(100%)"
-})
-
-
-
+sideMenuItems.addEventListener("click", function () {
+  sideMenuBar.style.transform = "translate(100%)";
+});
 
 // biography read more
 
 function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more"; 
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less"; 
-      moreText.style.display = "inline";
-    }
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
   }
+}
+
+// update date yearly
+// scripts.js
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the current year
+  var currentYear = new Date().getFullYear();
+  // Find the element with the id "current-year" and set its text content to the current year
+  document.getElementById("current-year").textContent = currentYear;
+});
